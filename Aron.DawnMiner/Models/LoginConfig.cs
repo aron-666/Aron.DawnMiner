1115205c6afe5644ad814404caffc1f3de6f9173
Aron <aronkao0115@gmail.com>
2024-11-22 07:52:33 +0800

    update

--- Aron.DawnMiner/Models/LoginConfig.cs
+++ Aron.DawnMiner/Models/LoginConfig.cs
@@ -3,18 +3,18 @@
     public class LoginConfig
     {
         public string discordid { get; set; }
-        public int discordid_points { get; set; }
+        public decimal discordid_points { get; set; }
         public string firstname { get; set; }
         public string lastUpdatedTime { get; set; }
         public string password { get; set; }
         public string referralCode { get; set; }
-        public int refrral_points { get; set; }
+        public decimal refrral_points { get; set; }
         public string telegramid { get; set; }
-        public int telegramid_points { get; set; }
+        public decimal telegramid_points { get; set; }
         public string token { get; set; }
-        public int total_points { get; set; }
+        public decimal total_points { get; set; }
         public string twitter_x_id { get; set; }
-        public int twitter_x_id_points { get; set; }
+        public decimal twitter_x_id_points { get; set; }
         public string username { get; set; }
         public string wallet { get; set; }
     }
